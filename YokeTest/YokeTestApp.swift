c48da03fad882567283ad330f40fc308e78ccfda
David Jeong <yechanjeong@yahoo.com>
2022-01-12 14:33:49 -0500

    Initial Commit

---
+++ YokeTest/YokeTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  YokeTestApp.swift
+//  YokeTest
+//
+//  Created by David Jeong on 1/12/22.
+//
+
+import SwiftUI
+
+@main
+struct YokeTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
